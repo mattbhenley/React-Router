edc6cb534dc062e579e866ebb045baea2d98283e
matt henley <mattbhenley@gmail.com>
2019-05-22 17:39:50 -0500

    Add files via upload

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+
+    useEffect( () => {
+        fetchItems();
+    },[]);
+
+    const [items, setItems] = useState([]);
+
+    const fetchItems = () => {
+        const data = await fetch ('https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get');
+
+        const items = await data.json();        
+        console.log(items);
+        setItems(items);
+    };
+
+
     return ( 
-    <div className = "App" >
-        <h1 >Shop</h1> 
-        </div>
+    <div>
+        {items.map(item => (
+            <h1>{item.name}</h1>
+        ))}    
+     </div>
     );
 }
 
