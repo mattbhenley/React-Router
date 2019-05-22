76a4381663a36233841b05ef76016c379cf8fa9d
matt henley <mattbhenley@gmail.com>
2019-05-22 17:29:10 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
-//import { BrowerserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowerserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <About />
-      <Shop />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+
+const Home =() => (
+  <div>
+    <h1>Homepage</h1>
+  </div>
+);
+
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
+import {link} from 'react-router-dom';
 
 function Nav() {
+    const style = {
+        color: 'white'
+    };
+
+
     return (
         <nav class = "Nav">
             <h3>Logo</h3>
             <ul className = "nav-links">
+                <Link style={navStyle} to=' /about'>
                 <li>About</li>
+                </Link>
+                <Link style={navStyle} to= '/shop'>
                 <li>Shop</li>
+                </Link>
             </ul>
         </nav>
     );
